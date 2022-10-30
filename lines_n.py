bcff30f943b4b1973adf5ae374209e752401f4fb
NASA-0007 <116829149+NASA-0007@users.noreply.github.com>
2022-10-30 19:34:39 +0530

    Update lines_n.py

--- lines_n.py
+++ lines_n.py
@@ -1,12 +1,11 @@
 def length():
-    f = open("C:\\Users\\navik\\Downloads\\School\\Text.txt")
-    for i, l in enumerate(f):
-        pass
-    return i+1
-
+    f=open("C:\\Users\\navik\\Downloads\\School\\Data1.txt")
+    k=f.readlines()
+    g=len(k)
+    return g
 
 def text():
-    f = open("C:\\Users\\navik\\Downloads\\School\\Text.txt")
+    f = open("C:\\Users\\navik\\Downloads\\School\\Data1.txt")
     g = length()
     while g > 0:
         d=f.readline()
