a205b3fc75c536a7c29ddc2a6fdad236d6744525
NASA-0007 <116829149+NASA-0007@users.noreply.github.com>
2022-11-08 01:44:50 -0800

    Update Stack Hostel.py

--- Stack Hostel.py
+++ Stack Hostel.py
@@ -16,17 +16,46 @@ def delete(stack):
         else:
             top=len(stack)-1
         return i
-    
-def display(stack):
-    a=empty(stack)
-    if a == "underflow":
+def empty(stack):
+    if len(stack)==0:
+        return "underflow"
+
+def add(w,x,y,a,b,c):
+    w.append(a)
+    x.append(b)
+    y.append(c)
+    top=len(w)-1
+def delete(stack):
+    a=empty(stack,)
+    if a=="underflow":
         print("The Stack is Empty")
     else:
-        a=len(stack)-1
-        for i in range(a):
-            print (stack[a])
-            print()
-stack=[]
+        i=stack.pop()
+        if len(stack)==0:
+            top=None
+        else:
+            top=len(stack)-1
+        return i
+    
+def display(x,y,z):
+    a=empty(x)
+    b=empty(y)
+    c=empty(z)
+    if a == "underflow" and b== "underflow" and c== "underflow":
+        print("The Stack is Empty")
+    else:
+        aa=len(x)-1
+        bb=len(y)-1
+        cc=len(z)-1
+        print("Hostel.No""|""Students""|""Rooms")
+        while aa>=0 and bb>=0 and cc>=0:
+            print(x[aa],"   ",y[bb],"   ",z[cc])
+            aa-=1
+            bb-=1
+            cc-=1
+Hostel=[]
+Students=[]
+Rooms=[]
 top=None
 while True:
     print("1:Add")
@@ -38,7 +67,7 @@ while True:
         i=int(input("Enter the Hostel Number"))
         j=int(input("Enter the Total Number of Students"))
         k=int(input("Enter the Total Number of Rooms"))
-        add(stack,i,j,k)
+        add(Hostel,Students,Rooms,i,j,k)
     elif a==2:
         i=delete(stack)
         if i=="underflow":
@@ -46,8 +75,9 @@ while True:
         else:
             print("Element Deleted",i)
     elif a==3:
-        display(stack)
+        display(Hostel,Students,Rooms)
     elif a==4:
         break
     else:
         print("Enter a Valid Input")
+
