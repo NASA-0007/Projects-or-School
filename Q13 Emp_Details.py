af48f04351db271b2c0af7f1430422fa4cccf4ba
NASA-0007 <116829149+NASA-0007@users.noreply.github.com>
2022-11-08 20:07:49 -0800

    Create Q13 Emp_Details.py

---
+++ Q13 Emp_Details.py
@@ -0,0 +1,50 @@
+import csv
+def menu():
+    while True:
+        a=print("1.Create CSV File")
+        b=print("2.Search")
+        c=print("3.Exit")
+        print()
+        ch=int(input("Enter a Choice: "))
+        if ch==1:
+            f=open("emp_det.csv","w")
+            empdet=csv.writer(f)
+            empdet.writerow(["Dept.ID","Name","City"])
+            print()
+            k=int(input("Number of Datas to be Added: "))
+            print()
+            
+            for i in range(k):
+                aa=input("Department Number: ")
+                bb=input("Name: ")
+                cc=input("City: ")
+                print()
+                dd=([aa,bb,cc])
+                empdet.writerow(dd)
+            f.close()
+        elif ch==2:
+            fh=open("emp_det.csv")
+            print()
+            kk=input("Enter the Department ID to be Searched: ")
+            empread=csv.reader(fh)
+            for j in empread:
+                gg=len(j)
+                while gg%10!=0:
+                    g = j[0]
+                    k = j[1]
+                    h = j[2]
+                    gg-=1
+            if kk==g:
+                print()
+                print("Department ID:",g,"Name:",k,"City:",h)
+                print()
+            else:
+                print()
+                print("The Department ID doesn't Exist")
+                print()
+            fh.close()
+        elif ch==3:
+            break
+        else:
+            print("Enter a Valid Input")
+menu()
