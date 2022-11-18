2a96b479782e0d5673e971aac56dfde408963098
NASA-0007 <116829149+NASA-0007@users.noreply.github.com>
2022-11-17 22:12:13 -0800

    Update Reverse.py

--- Reverse.py
+++ Reverse.py
@@ -5,41 +5,61 @@ def empty(stack):
 def add(w,a):
     w.append(a)
     top=len(w)-1
-
 def reverse(x):
     a=empty(x)
     if a == "underflow":
         return "underflow"
     else:
         aa=len(x)
-        for i in range(aa):
-            k=x.pop()
+        b=0
+        while aa!=b:
+            k=x.pop(-1)
+            b+=1
+            if len(x)==0:
+                top=None
+            else:
+                top=len(x)-1
             return k
-        if len(x)==0:
-            top=None
-        else:
-            top=len(x)-1
 string=[]
 top=None
 while True:
     print("1:Add")
-    print("2:Delete")
+    print("2:Reverse")
     print("3:Exit")
     a=int(input("Enter a Choice: "))
     print()
     if a==1:
         i=input("Enter the String: ")
-        hh=[i]
-        for j in range(len(hh)):
-            add(string,hh[j])
+        tt=0
+        hh=[*i]
+        
+        while len(hh)!=tt:
+            sa=hh[tt]
+            add(string,sa)
+            tt+=1
+        qq=(" ")
+        string.append(qq)
+        
     elif a==2:
         pp=reverse(string)
+        ww=len(string)
+        gg=0
+        got=True
         if pp=="underflow":
             print("Stack is Empty")
+            got=False
             print()
-        else:
-            for i in range(len(hh)):
-                print("Element Deleted",i)
+        elif got==True:
+            x=[]
+            while ww!=gg:
+                uu=reverse(string)
+                x.append(uu)
+                gg+=1
+            d=""
+            for lu in x:
+                d+=lu
+            print(d)
+                
     elif a==3:
         break
     else:
